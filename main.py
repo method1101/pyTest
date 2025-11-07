a8f47ebb63dfb7325ef8c50dbfd79e155a40cd46
Koorosh <method1101@gmail.com>
2025-11-07 19:31:29 +0330

    nesting example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-alien_0 = {'color': 'green', 'points': 5}
-alien_1 = {'color': 'yellow', 'points': 10}
-alien_2 = {'color': 'red', 'points': 15}
-aliens = [alien_0, alien_1, alien_2]
-for alien in aliens:
-	print(alien)
+pizza = {
+	'crust': 'thick',
+	'toppings': ['mushrooms', 'extra cheese'],
+}
+print("You ordered a " + pizza['crust'] + "-crust pizza " + "with the following toppings:")
+for topping in pizza['toppings']:
+	print("\t" + topping)
