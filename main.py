78c5894bed9d3eb8ae26e9b0985eb0ec195cfb21
Koorosh <method1101@gmail.com>
2025-11-07 19:29:20 +0330

    nesting

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-favorite_languages = {
-	'jen': 'python',
-	'sarah': 'c',
-	'edward': 'ruby',
-	'phil': 'python',
-}
-print("The following languages have been mentioned:")
-for language in set(favorite_languages.values()):
-	print(language.title())
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+	print(alien)
