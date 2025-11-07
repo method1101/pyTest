9edb6c2079cdda354d4797f3b3818fb3fc552612
Koorosh <method1101@gmail.com>
2025-11-07 19:35:12 +0330

    nesting example 2

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-pizza = {
-	'crust': 'thick',
-	'toppings': ['mushrooms', 'extra cheese'],
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
 }
-print("You ordered a " + pizza['crust'] + "-crust pizza " + "with the following toppings:")
-for topping in pizza['toppings']:
-	print("\t" + topping)
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
