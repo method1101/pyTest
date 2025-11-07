ac7c0843614f135807cf483338d9b34e772238e6
Koorosh <method1101@gmail.com>
2025-11-07 19:37:50 +0330

    input processing 1

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-users = {
-    'aeinstein': {
-        'first': 'albert',
-        'last': 'einstein',
-        'location': 'princeton',
-    },
-
-    'mcurie': {
-        'first': 'marie',
-        'last': 'curie',
-        'location': 'paris',
-    },
-}
-for username, user_info in users.items():
-    print("\nUsername: " + username)
-    full_name = user_info['first'] + " " + user_info['last']
-    location = user_info['location']
-    print("\tFull name: " + full_name.title())
-    print("\tLocation: " + location.title())
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
