dee99b14a1d89e9e673da14d6485a2687a7fd251
Koorosh <method1101@gmail.com>
2025-11-08 13:47:21 +0330

    what is a flag

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-current_number = 1
-while current_number <= 5:
-	print(current_number)
-	current_number += 1
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+active = True  # Flag = True
+while active:
+	message = input(prompt)
+	if message == 'quit':
+		active = False
+	else:
+		print(message)
