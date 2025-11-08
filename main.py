36aca5ce04f791e724cdf179ab48684939f5b3c9
Koorosh <method1101@gmail.com>
2025-11-08 17:47:34 +0330

    Positional arguments in functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet_user(username):
-    print("Hello, " + username.title() + "!")
-
-greet_user('jesse')
+def describe_pet(animal_type, pet_name):
+	"""Display information about a pet."""
+	print("\nI have a " + animal_type + ".")
+	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+describe_pet('hamster', 'harry')
