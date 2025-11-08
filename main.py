527e1ab6d9bccab69d9414c05bf5bd8f4b5e5b42
Koorosh <method1101@gmail.com>
2025-11-08 18:08:20 +0330

    Return statements in functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def describe_pet(animal_type, pet_name):
-    """Display information about a pet."""
-    print("\nI have a " + animal_type + ".")
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-
-
-describe_pet(animal_type='hamster', pet_name='harry')
+def get_formatted_name(first_name, last_name):
+	"""Return a full name, neatly formatted."""
+	full_name = first_name + ' ' + last_name
+	return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
