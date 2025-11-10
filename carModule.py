54092714b0b51ee6e883a2ee34fb3841a3a32e62
Koorosh <method1101@gmail.com>
2025-11-10 12:45:40 +0330

    Introduction to modules

---
+++ carModule.py
@@ -0,0 +1,2 @@
+def make_car(tires):
+    print(f"Making a car having {tires} tires.")
--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def make_pizza(*toppings):
-    """Summarize the pizza we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print("- " + topping)
+import carModule
 
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+carModule.make_car(3)
