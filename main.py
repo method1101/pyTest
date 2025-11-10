f8583f28c78a733ac79832bee599461ef3b12440
Koorosh <method1101@gmail.com>
2025-11-10 12:36:58 +0330

    Arbitrary number of arguments passed to a function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def greet_users(names):
-    """Print a simple greeting to each user in the list."""
-    for name in names:
-        msg = "Hello, " + name.title() + "!"
-        print(msg)
-usernames = ['hannah', 'ty', 'margot']
-greet_users(usernames)
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
