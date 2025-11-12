622383b107b9161ef3d77b81dfe82417f7f27694
Koorosh <method1101@gmail.com>
2025-11-12 14:41:42 +0330

    Reading from a file

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-""" Learning Classes in Python """
-
-class Employee:
-  def __init__(self, fname, lname):
-    self.fname = fname
-    self.lname = lname
-  def greet(self):
-    return f'Hello {self.fname} {self.lname}'
-
-emp1 = Employee('John', 'Smith')
-print(emp1.greet())
-print(Employee.greet(emp1))
+with open("./flower") as file:
+    contents = file.read()
+    print(contents)
