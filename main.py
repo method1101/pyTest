86b42374e446003f44d676adaa2c251ce5134d9c
Koorosh <method1101@gmail.com>
2025-11-16 18:18:28 +0330

    Printing file contents having no extra new lines using a class call.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-with open("./flower") as file:
-    for line in file:
-        print(line.rstrip())
+
+class FileReader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_file(self):
+        with open(self.filename) as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line.rstrip())
+
+filereader = FileReader("flower")
+filereader.read_file()
